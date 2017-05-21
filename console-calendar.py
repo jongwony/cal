ca6f4aa204ed8b994920e492c6a6f0ee17d865b7
Jongwon <lastone9182@gmail.com>
2017-05-21 16:01:56 +0900

    fix typo and refactor

--- console-calendar.py
+++ console-calendar.py
@@ -8,43 +8,43 @@ import os
 import urllib
 
 def push_event(config):
-  session = GoogleCal(venv.SCRIPTDIR)
-  session.build_calendar()
+    session = GoogleCal(venv.SCRIPTDIR)
+    session.build_calendar()
 
-  event = {
-    'summary': config.summary,
-    'location': config.location,
-    'description': config.description,
-    'start': {
-      'dateTime': config.start_date_format(config.date_eventform),
-      'timeZone': 'Asia/Seoul',
-    },
-    'end': {
-      'dateTime': config.end_date_format(config.date_eventform),
-      'timeZone': 'Asia/Seoul',
-    },
-    'recurrence': [
-      'RRULE:FREQ=DAILY;COUNT=' + config.duration_d
-    ],
-  #   'attendees': [
-  #     {'email': 'lpage@example.com'},
-  #     {'email': 'sbrin@example.com'},
-  #   ],
-    'reminders': {
-      'useDefault': False,
-      'overrides': [
-        {'method': 'email', 'minutes': 24 * 60},
-        {'method': 'popup', 'minutes': config.alarm},
-      ],
-    },
-  }
+    event = {
+        'summary': config.summary,
+        'location': config.location,
+        'description': config.description,
+        'start': {
+            'dateTime': config.start_datetime_format(config.date_eventform),
+            'timeZone': 'Asia/Seoul',
+        },
+        'end': {
+            'dateTime': config.end_datetime_format(config.date_eventform),
+            'timeZone': 'Asia/Seoul',
+        },
+    # 'recurrence': [
+    #   'RRULE:FREQ=DAILY;COUNT=' + config.duration_d
+    # ],
+    # 'attendees': [
+    #   {'email': 'lpage@example.com'},
+    #   {'email': 'sbrin@example.com'},
+    # ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': config.alarm},
+            ],
+        },
+    }
 
-  session.insert_event(event)
+    session.insert_event(event)
 
 def make_ics(config):
-  uid = time.strftime('%Y-%m-%d_%H%M%S')
+    uid = time.strftime('%Y-%m-%d_%H%M%S')
 
-  ics_format = """BEGIN:VCALENDAR
+    ics_format = """BEGIN:VCALENDAR
 VERSION:2.0
 CALSCALE:GREGORIAN
 METHOD:PUBLISH
@@ -63,130 +63,128 @@ END:VALARM
 END:VEVENT
 END:VCALLENDAR""".format(
     uid,
-    config.start_date_format(config.date_linkform),
-    config.end_date_format(config.date_linkform),
+    config.start_datetime_format(config.date_linkform),
+    config.end_datetime_format(config.date_linkform),
     config.location,
     config.summary,
     config.description,
     config.alarm,
     config.description
-  )
+    )
 
-  # binary encoding
-  ics_format = ics_format.encode('utf-8')
+    # binary encoding
+    ics_format = ics_format.encode('utf-8')
 
-  # make ics
-  filename = uid + '.ics'
-  abspath = os.path.join(venv.SCRIPTDIR, filename)
-  with open(abspath, 'wb') as f:
-      f.write(ics_format)
+    # make ics
+    filename = uid + '.ics'
+    abs_path = os.path.join(venv.SCRIPTDIR, filename)
+    with open(abs_path, 'wb') as f:
+        f.write(ics_format)
 
-  #os.system('start ' + abspath)
-  return abspath
+    #os.system('start ' + abspath)
+    return abs_path
 
 def google_link(config):
-  uri = 'http://www.google.com/calendar/event'
-  param = {
-    'action': 'TEMPLATE',
-    'text': config.summary,
-    'dates': '/'.join([
-    config.start_date_format(config.date_linkform),
-    config.end_date_format(config.date_linkform)
-    ]),
-    'details': config.description,
-    'location': config.location,
-  }
+    uri = 'http://www.google.com/calendar/event'
+    param = {
+        'action': 'TEMPLATE',
+        'text': config.summary,
+        'dates': '/'.join([
+            config.start_datetime_format(config.date_linkform),
+            config.end_datetime_format(config.date_linkform)
+            ]),
+        'details': config.description,
+        'location': config.location,
+    }
 
-  link = uri + '?' + urllib.parse.urlencode(param)
-  return link
+    result_link = uri + '?' + urllib.parse.urlencode(param)
+    return result_link
 
 def write_diary(config):
-  session = GoogleCal(venv.SCRIPTDIR)
-  session.build_calendar()
+    session = GoogleCal(venv.SCRIPTDIR)
+    session.build_calendar()
 
-  event = {
-    'summary': config.summary,
-    'description': config.description,
-    'start': {
-      'dateTime': config.start_date_format(config.date_eventform),
-      'timeZone': 'Asia/Seoul',
-    },
-    'end': {
-      'dateTime': config.end_date_format(config.date_eventform),
-      'timeZone': 'Asia/Seoul',
+    event = {
+        'summary': config.summary,
+        'description': config.description,
+        'start': {
+            'date': config.start_datetime_format(config.date_diaryform)
+        },
+        'end': {
+            'date': config.end_datetime_format(config.date_diaryform),
+        }
     }
-  }
-  session.insert_event(event)
+    session.insert_event(event)
 
 def quick_init():
-  query = input('Query: ')
-  return query
+    query = input('Query: ')
+    return query
 
 def quick_add(query):
-  session = GoogleCal(venv.SCRIPTDIR)
-  session.build_calendar()
-  session.quick_event(query)
+    session = GoogleCal(venv.SCRIPTDIR)
+    session.build_calendar()
+    session.quick_event(query)
 
 def call_list():
-  session = GoogleCal(venv.SCRIPTDIR)
-  session.build_calendar()
-  #session.calendar_list_all()
-  session.calendar_lists()
+    session = GoogleCal(venv.SCRIPTDIR)
+    session.build_calendar()
+    #session.calendar_list_all()
+    session.calendar_lists()
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-i', help='Make ics file', action='store_true')
-  parser.add_argument('-d', help='Write diary', action='store_true')
-  parser.add_argument('-g', help='Google calendar link', action='store_true')
-  parser.add_argument('--no-push', help="Don't push my calendar", action='store_false')
-  parser.add_argument('-q', help='Push quickAdd calendar', action='store_true')
-  parser.add_argument('-l', help='Calendar lists', action='store_true')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', help='Make ics file', action='store_true')
+    parser.add_argument('-d', help='Write diary', action='store_true')
+    parser.add_argument('-g', help='Google calendar link', action='store_true')
+    parser.add_argument('--no-push', help="Don't push my calendar", action='store_false')
+    parser.add_argument('-q', help='Push quickAdd calendar', action='store_true')
+    parser.add_argument('-l', help='Calendar lists', action='store_true')
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  if args.l:
-    call_list()
-    quit()
+    if args.l:
+        call_list()
+        quit()
 
-  # Quick Add Calendar
-  if args.q:
-    query = quick_init()
-    quick_add(query)
-    print('Event created.')
-    quit()
+    # Quick Add Calendar
+    if args.q:
+        query = quick_init()
+        quick_add(query)
+        print('Event created.')
+        quit()
 
-  # Initialize
-  config = Configdata()
+    # Initialize
+    config = Configdata()
 
-  date = input('날짜: ')
-  assert date is not '', '날짜가 없습니다.'
-  summary = input('제목: ')
-  assert summary is not '', '제목이 없습니다.'
-  description = input('세부정보: ')
+    date = input('시간: ')
+    assert date is not '', '시간이 없습니다.'
+    summary = input('제목: ')
+    assert summary is not '', '제목이 없습니다.'
+    duration = input('기간: ')
+    description = input('세부정보: ')
 
-  # Diary
-  if args.d:
-    config.diary_triming(date, summary, description)
-    write_diary(config)
-    print('Event created.')
-    quit()
 
-  location = input('위치: ')
-  h_duration = input('기간(시간): ')
-  d_duration = input('기간(일): ')
-  alarm = input('알람(분 전): ')
-  config.triming(date, summary, description, location, h_duration, d_duration, alarm)
+    # Diary
+    if args.d:
+        config.diary_triming(date, summary, description, duration)
+        write_diary(config)
+        print('Event created.')
+        quit()
 
-  # ICS file
-  if args.i:
-    abspath = make_ics(config)
-    print('ics file created at' + abspath)
+    location = input('위치: ')
+    alarm = input('알람(분 전): ')
+    config.triming(date, summary, description, location, duration, alarm)
 
-  # Google Calendar link
-  if args.g:
-    link = google_link(config)
-    print('google link: ' + link)
-  
-  if args.no_push:
-    push_event(config)
-    print('Event created.')
+    # ICS file
+    if args.i:
+        abs_path = make_ics(config)
+        print('ics file created at' + abs_path)
+
+    # Google Calendar link
+    if args.g:
+        result_link = google_link(config)
+        print('google link: ' + result_link)
+
+    if args.no_push:
+        push_event(config)
+        print('Event created.')
--- inputconfig.py
+++ inputconfig.py
@@ -4,6 +4,11 @@ import re
 from datetime import datetime, timedelta
 from collections import OrderedDict
 
+def query_trim(patch, query, els):
+    refine = re.findall(patch, query)
+    assert len(refine) <= 1, 'duplicate' + patch
+    return refine[0] if refine else els
+
 def date_triming(date):
     """{'year':int, 'month':int, 'day':int, 'hour':int, 'minute':int, 'locale':string}"""
     trim_date = OrderedDict()
@@ -12,42 +17,34 @@ def date_triming(date):
     now = datetime.today()
 
     # parse criteria
-    noon_criteria = {'오전': 'AM', '오후': 'PM'}
-    nearday = {'오늘': 0, '내일': 1, '모레': 2}
+    noon_criteria = {'오전': 'AM', '오후': 'PM', 'AM': 'AM', 'PM': 'PM'}
+    nearday = {'어제': -1, '오늘': 0, '내일': 1, '모레': 2}
 
     # ex) 내일 오후 3시 = 2017년 5월 17일 3시 0분 PM
-    trim_date['year'] = str(now.year)
+    trim_date['year'] = format(query_trim(r'(\d+)년', date, now.year))
+    trim_date['month'] = format(query_trim(r'(\d+)월', date, now.month))
+    trim_date['day'] = format(query_trim(r'(\d+)일', date, now.day))
 
-    month = re.findall(r'(\d+)월', date)
-    assert len(month) <= 1, 'duplicate 월'
-    trim_date['month'] = format(month[0]) if month else format(now.month)
+    # default time 7:00 AM
+    trim_date['hour'] = format(query_trim(r'(\d+)시', date, 7))
+    trim_date['minute'] = format(query_trim(r'(\d+)분', date, 0))
 
-    day = re.findall(r'(\d+)일', date)
-    assert len(day) <= 1, 'duplicate 일'
-    trim_date['day'] = format(day[0]) if day else format(now.day)
-
-    # default time 7AM
-    hour = re.findall(r'(\d+)시', date)
-    assert len(hour) <= 1, 'duplicate 시'
-    trim_date['hour'] = format(hour[0]) if hour else '7'
-
-    minute = re.findall(r'(\d+)분', date)
-    assert len(hour) <= 1, 'duplicate 분'
-    trim_date['minute'] = format(minute[0]) if minute else '0'
-
-    locale = re.findall(r'(오전|오후|AM|PM)', date)
-    assert len(locale) <= 1, 'duplicate locale'
-    trim_date['locale'] = noon_criteria[locale[0]] if locale else 'AM'
+    trim_date['locale'] = noon_criteria[query_trim(r'(오전|오후|AM|PM)', date, 'AM')]
 
     # make datetime
     start_date = datetime.strptime('/'.join(trim_date.values()), '/'.join(['%Y', '%m', '%d', '%I', '%M', '%p']))
 
-    near = re.findall(r'(오늘|내일|모레)', date)
-    assert len(near) <= 1, 'duplicate near date'
-    near_days = int(nearday[near[0]]) if near else 0
+    near_days = int(query_trim(r'(어제|오늘|내일|모레)', date, 0))
     near_delta = timedelta(days=near_days)
 
     return start_date + near_delta
+
+def duration_datetime(date):
+    day = int(query_trim(r'(\d+)일', date, 1)) - 1
+    hour = query_trim(r'(\d+)시', date, 1)
+    minute = query_trim(r'(\d+)분', date, 0)
+
+    return timedelta(days=day, hours=hour, minutes=minute)
 
 class Configdata:
     def __init__(self):
@@ -55,24 +52,25 @@ class Configdata:
         self.summary = ''
         self.location = ''
         self.description = ''
-        self.h_duration = ''
-        self.d_duration = ''
+        self.duration = ''
         self.alarm = ''
 
     def diary_triming(self, *args):
-        self.date, self.summary, self.description = args
+        self.date, self.summary, self.description, self.duration = args
         self.description = '스크립트에 의해 자동 생성된 이벤트입니다.' if self.description is '' else self.description
         self.date = date_triming(self.date)
+        self.duration = duration_datetime(self.duration)
 
     def triming(self, *args):
-        self.date, self.summary, self.description, self.location, self.duration_h, self.duration_d, self.alarm = args
+        self.date, self.summary, self.description, self.location, self.duration, self.alarm = args
         self.alarm = 10 if self.alarm is '' else int(self.alarm)
-        self.duration_h = 1 if self.duration_h is '' else int(self.duration_h)
-        self.duration_d = '1' if self.duration_d is '' else self.duration_d
         self.description = '스크립트에 의해 자동 생성된 이벤트입니다.' if self.description is '' else self.description
         self.date = date_triming(self.date)
+        self.duration = duration_datetime(self.duration)
 
-
+    @property
+    def date_diaryform(self):
+        return '%Y-%m-%d'
 
     @property
     def date_linkform(self):
@@ -82,9 +80,9 @@ class Configdata:
     def date_eventform(self):
         return '%Y-%m-%dT%H:%M:00'
 
-    def start_date_format(self, form):
+    def start_datetime_format(self, form):
         return datetime.strftime(self.date, form)
 
-    def end_date_format(self, form):
-        end_date = self.date + timedelta(hours=self.duration_h)
+    def end_datetime_format(self, form):
+        end_date = self.date + self.duration
         return datetime.strftime(end_date, form)
